f6f8f778ab5251225a291dffc3d685f494252fc3
abhishekturukmane <abhishekturukmaneofficial98@gmail.com>
2022-10-30 07:48:55 -0700

    Added: added UC2 Both lines are Equal or Not

--- src/com/bridgelabz/LineCompressionUC1.java
+++ src/com/bridgelabz/LineCompressionUC1.java
@@ -4,24 +4,50 @@ import java.util.Scanner;
 
 public class LineCompressionUC1 {
 
+	//uc2 ---Length of line is equal or not
 	public void lineCompare()
 	{
-		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter x1 ,y1 and x2 ,y2 co-ordinates:");
+		System.out.println("Enter co-ordinates for Line1:");
 		int x1=sc.nextInt();
 		int y1=sc.nextInt();
 		int x2=sc.nextInt();
 		int y2=sc.nextInt();
+		System.out.println("Enter co-ordinates for Line2:");
+		int m1=sc.nextInt();
+		int n1=sc.nextInt();
+		int m2=sc.nextInt();
+		int n2=sc.nextInt();
 		
-		System.out.print("The 2 points of X and Y co-ordinates are:"+ "("+ x1+","+y1+") ,"+"("+x2+","+y2+")");
-		System.out.println();
-		//length of line
-		double length1=Math.pow(x2-x1, 2);
-		double length2=Math.pow(y2-y1, 2);
-		double sum=(length1+length2);
-		double sq=Math.sqrt(sum);
-		System.out.println("The length of line is:"+ sq);
+		//calculate length for line 1
+		double power1=Math.pow(x2-x1, 2);//square
+		double power2=Math.pow(y2-y1, 2);
+		double sum1=(power1+power2);
+		double sq1=Math.sqrt(sum1);
+		System.out.println("The Length of Line 1 is :"+ sq1);
+		String a1=String.valueOf(sq1);
+
+		//calculate length for line 2
+		double power3=Math.pow(m2-m1, 2);
+		double power4=Math.pow(n2-n1, 2);
+		double sum2=(power3+power4);
+		double sq2=Math.sqrt(sum2);
+		System.out.println("The Length of Line 2 is:"+ sq2);
+		String a2=String.valueOf(sq2);// String.valuOf is used to conv double to string
+		
+//		double sq1=Math.sqrt((x2-x1^2)+(y2-y1^2));
+//		int i=(int)sq1;
+//		double sq2=Math.sqrt((m2-m1^2)+(n2-n1^2));
+//		int j=(int)sq2;
+		//equating both the lines
+		if(a1.equals(a2))
+		{
+			System.out.println("Both the Lines are Equal");
+		}
+		else
+		{
+			System.out.println("Both the lines are not Equal");
+		}
 	}
 	public static void main(String[] args) {
 		LineCompressionUC1 obj=new LineCompressionUC1();
